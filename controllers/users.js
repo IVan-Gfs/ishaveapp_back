106a145b7f906f97054a44535335f73de1b54005
IVan-Gfs <ivangfs2004@gmail.com>
2023-05-28 20:45:44 -0300

    CadastroSemifinalizado

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,11 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const {encryptPassword, verifyPassword, gerarURL, decryiptURL} = require('./cripto.js')
+const { encryptPassword, verifyPassword, gerarURL, decryiptURL } = require('./cripto.js')
 const transporter = require('../config/mailprovider.js')
 const fs = require('fs')
 
 module.exports = {
-  async sendData(req, res){
+  async sendData(req, res) {
 
     dados = req.body;
     dados.usuario.nomeUsuario = dados.empresa.nomeEmpresa
@@ -14,9 +14,9 @@ module.exports = {
 
     const dataAtual = new Date();
     const experationTime = 0.25 * 60 * 60 * 1000;
-    const expirationDate = new Date(dataAtual.getTime()+experationTime)
+    const expirationDate = new Date(dataAtual.getTime() + experationTime)
     const urlRecord = await prisma.urlConfirm.create({
-      data:{
+      data: {
         url: gerarURL(dados),
         expires_at: expirationDate.toISOString()
       }
@@ -38,54 +38,76 @@ module.exports = {
             </html>`,
     };
 
-    transporter.sendMail(mailOptions, (error, info)=>{
-        if (error) {
-            console.log(error);
-            res.send("<h2>E-mail inválido.<h2>")
-          } else {
-            console.log('Email sent: ' + info.response);
-            next()
-          }
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.send("<h2>E-mail inválido.<h2>")
+      } else {
+        console.log('Email sent: ' + info.response);
+        next()
+      }
     });
     res.send('Enviamos um email de confirmação, verifique-o.')
   },
   async register(req, res) {
+    
 
-  const urlRecord = await prisma.urlConfirm.findMany({
-    where:{
-      url:{
-        equals: req.body.url.substring('7')
+    //Antes do cadastro, desencripitar os dados:
+    const todosDados = decryiptURL(req.body.dataQuery.encryptedData, req.body.dataQuery.iv)
+    console.log(todosDados)
+    const usuarioExistente = await prisma.usuarios.count({
+      where: {
+        nomeUsuario: todosDados.usuario.nomeUsuario,
+        emailUsuario: todosDados.usuario.emailUsuario,
+        senhaUsuario: todosDados.usuario.senhaUsuario
+      }
+    })
+    var message = '';
+    var registered = true
+    if (usuarioExistente > 0) {
+      message = "Usuário já foi cadastro, realize o "
+    } else {
+      //Buscar por url no banco para determinar se está válida.
+      const urlRecord = await prisma.urlConfirm.findMany({
+        where: {
+          url: {
+            equals: req.body.url.substring('7')
+          }
+        }
+      });
+
+      //Converter datas para dados numéricos para comparação
+      const dataAtual = new Date().getTime();
+      const expiresDate = Date.parse(urlRecord[0].expires_at)
+
+      //Tratativa final para registro de dados e resposta 
+      if (expiresDate > dataAtual) {
+        // Realizar cadastro ---
+        const endereco = await prisma.endereco.create({ data: todosDados.endereco })
+        todosDados.empresa.enderecoId = endereco.idEndereco
+        const empresa = await prisma.empresa.create({ data: todosDados.empresa })
+        todosDados.usuario.empresaId = empresa.idEmpresa
+        const usuario = await prisma.usuarios.create({ data: todosDados.usuario })
+
+        if (endereco && empresa && usuario) {
+          message = 'Cadastro Confirmado com sucesso, realize o '
+        } else {
+          message = 'Algo inesperado aconteceu.'
+          registered = false
+        }
+      } else {
+        registered = false
+        message = 'Ops.. Você demorou demais para confirmar seu cadastro. Realize o cadastro novamente.'
       }
     }
-  });
- 
-   const dataAtual = new Date().getTime();
-   const expiresDate = Date.parse(urlRecord[0].expires_at)
-   dados = JSON.stringify(urlRecord[0])
 
-   var message = '';
-   if(expiresDate > dataAtual){
-    message = 'Cadastro Confirmado com sucesso, realize o login.'
-    
-    //Antes do cadastro, desencripitar os dados:
-    const todosDados = decryiptURL(req.body.dados.dados, req.body.dados.iv)
-    console.log(todosDados)
-    // Realizar cadastro ---
-    // const endereco = await prisma.endereco.create({data:{}})
-    // const empresa = await prisma.endereco.create({data:{}})
-    // const usuario = await prisma.usuarios.create({data: {}})
-  
-   
-   }else{
-    message = 'Ops.. Você demorou demais para confirmar seu cadastro. Realize o cadastro novamente.'
-   }
-   res.send({message})
+    res.send({ message, registered })
   },
   async userexists(req, res, next) {
     const qtdUserWithData = await prisma.usuarios.count({
       where: {
-        emailUsuario: req.body.emailUsuario,
-        nomeUsuario: req.body.nomeUsuario,
+        emailUsuario: req.body.usuario.emailUsuario,
+        nomeUsuario: req.body.usuario.nomeUsuario,
       },
     });
     console.log(qtdUserWithData);
@@ -98,17 +120,19 @@ module.exports = {
   },
   async otp(req, res, next) {
     var code = gerarOTP(6)
-    
-      prisma.codAuth.create({data:{
-        codigo: code, 
+
+    prisma.codAuth.create({
+      data: {
+        codigo: code,
         email: req.body.usuario.emailUsuario,
-      }})
+      }
+    })
   },
-  async verifyCod(req, res){
+  async verifyCod(req, res) {
     const cod = await prisma.codAuth.findMany()
-    if(cod[cod.length-1] === req.code){
+    if (cod[cod.length - 1] === req.code) {
       res.send("<h1>Código correto</h1>")
-    }else{
+    } else {
       res.send("<h1>Código inválido</h1>")
     }
 
