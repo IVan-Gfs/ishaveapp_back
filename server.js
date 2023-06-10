2971e51990a01a39ffdad4686d4a7fcf7cdb8a52
Daniel Cesar Soares <danielcesartds@gmail.com>
2023-06-10 13:30:25 -0300

    feat: cors

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const cors = require('cors')
+
 const express = require('express');
 const router = require('./routes');
 const session = require('express-session');
@@ -5,12 +7,9 @@ const session = require('express-session');
 
 const app = express();
 app.use(express.json())
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
-    res.setHeader('Access-Control_Allow-Origin', 'http://localhost:5173')
-    res.setHeader('Access-Control-Allow-Methods', 'POST','GET');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
     next()
 })
 app.use(session({
