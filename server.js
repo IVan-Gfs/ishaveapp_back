2efe8cc57cd410d1165efecd14f08e3a56633598
Lucas Soares <lucasc.trabalhos@gmail.com>
2023-06-18 21:52:02 -0300

    fix: server

--- server.js
+++ server.js
@@ -2,22 +2,19 @@ const express = require("express");
 const router = require("./routes");
 const session = require("express-session");
 const cors = require("cors");
-const serverless = require("serverless-http");
 
 const app = express();
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control_Allow-Origin", "http://localhost:5173");
   res.setHeader("Access-Control-Allow-Methods", "POST", "GET");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-
 app.use(
   session({
     secret: "dhsdbsh_pqouans_ramdomss",
@@ -29,9 +26,6 @@ app.use(
     },
   })
 );
-
 app.use(router);
 
 app.listen(8081);
-
-module.exports.handler = serverless(app);
